997ed63ccae33554f83cf18dbbb1a931edfa9854
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2020-12-16 12:53:49 +0200

    Compilation success.
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

--- contracts/Main.sol
+++ contracts/Main.sol
@@ -2,56 +2,68 @@
 
 pragma solidity ^0.7.0;
 
-import "./Token.sol";
 import "usingtellor/contracts/UsingTellor.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "./Token.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 
 // The contract is also an ERC20 token which holds the collateral currency.
 // It also holds the semi stable token state inside the `token` variable.
-contract Main is SafeERC20, UsingTellor {
+contract Main is ERC20, UsingTellor {
     using SafeMath for uint256;
 
-    address admin;
+    address             admin;
+    uint256 constant    pctPrecision = 10000; // 10000 = 100% , 1 = 0.01%
+    uint256 immutable   creationTimestamp = block.timestamp;
+
+    Token private token;
     
     uint256         collateralID; // The id on Tellor oracle where to check the collateral token price. 
     uint256         collateralPricePrecision; // The id on Tellor oracle where to check the collateral token price.
-    address payable collateralAddress; // The currency 
+    address payable collateralAddress;
     uint256         collateralThreshold;
+    uint            collateralPriceAge;
+    uint256         liquidationPenatly;
 
-    // Inflation rate per day in basis points(1bsp=0.01%, 100bsp = 1%). 
+    // Inflation rate in basis points(1bsp=0.01%, 100bsp = 1%).
     // Set the rate at which the token decreases value.
-    uint256             inflRate ; 
-    uint256             inflPeriod; // Inflation increase every day by inflRate value.
-    uint256 constant    pctPrecision = 10000; // 10000 = 100% , 1 = 0.01%
-    uint256             tokenPrice; // Pegged to USD 18 decimal points.
-    uint256             inflLastUpdate;
-    address payable     inflBeneficiary; // Where to send the tokens
-
-    token Token;
-
+    uint256         inflRate ; 
+    uint256         inflPeriod; // Inflation increase every day by inflRate value.
+    uint256         inflLastUpdate;
+    address payable inflBeneficiary; // Where to send the inflation tokens.
 
 
     constructor(
-        address payable _collateralAddress, 
+        address payable _collateralAddress,
         uint256 _collateralID,
         uint256 _collateralThreshold,
         uint256 _collateralPricePrecision,
+        uint256 _collateralPriceAge,
+        uint256 _liquidationPenatly,
         address payable _inflBeneficiary,
-        address payable TellorAddress) UsingTellor(TellorAddress)  {
+        address payable TellorAddress,
+        string memory collateralName,
+        string memory collateralSymbol,
+        string memory tokenName,
+        string memory tokenSymbol
+        ) UsingTellor(TellorAddress) ERC20(collateralName, collateralSymbol) withinPctRange(_liquidationPenatly) withinPctRange(_collateralThreshold)  {
 
         admin = msg.sender;
+        // TODO Check if token ID is supported by the oracle and returns the price in USD.
+        // For now assume that the contract creator knows what he is doing.
+        // The collateral ID needs to return the value in USD.
+        collateralID = _collateralID;
         collateralAddress = _collateralAddress;
         collateralThreshold = _collateralThreshold;
         collateralPricePrecision = _collateralPricePrecision;
+        collateralPriceAge = _collateralPriceAge;
+        liquidationPenatly = _liquidationPenatly;
         inflBeneficiary = _inflBeneficiary;
-        collateralID = _collateralID;
-        // TODO Check if token ID is supported by the oracle;
-        // For now hard code the accepted request IDs anly only accept those that are peged to USD.
         inflLastUpdate = block.timestamp;
         inflPeriod = 86400;
-        tokenPrice = 1e18;
+
+        token = new Token(tokenName, tokenSymbol);
     }
 
     modifier onlyAdmin {
@@ -62,9 +74,19 @@ contract Main is SafeERC20, UsingTellor {
         _;
     }
 
+    modifier withinPctRange(uint256 percent) {
+        require(_withinPctRange(percent),"collateral threshold not within allowed limits");
+        _;
+    }
+
+    function _withinPctRange(uint256 percent) internal pure returns(bool){
+        return percent>=0 && percent < pctPrecision;
+    }
+
     function depositCollateral(uint256 amount) external {
         require(amount > 0, "deposit amount 0");
-        SafeERC20(collateralTokenAddr).safeTransferFrom(msg.sender,address(this),amount);
+        ERC20(collateralAddress).increaseAllowance(address(this),amount);
+        ERC20(collateralAddress).transfer(address(this),amount);
     }
 
     // Anyone can deposit collateral, but only admin can withdraw.
@@ -73,77 +95,101 @@ contract Main is SafeERC20, UsingTellor {
     // he should be allowed to mint up to the maximum amount based on his collateral deposit share.
     // Otherwise lets say a provider deposits 1ETH and mints all tokens to himself can drain the collateral of all providers.
     function withdrawCollateral(uint256 amount) external onlyAdmin{
-
+        _burn(msg.sender,amount);
+        require(collateralUtilization() < collateralThreshold,"withdraw will result the collateral utilizatoin below the collateral threshold");
+        this.transfer(msg.sender, amount);
     }
 
-    function withdrawTokens(uint256 amount) external returns (uint256){
+    function withdrawTokens(uint256 amount) external{
         require(amount>0,"amount should be greater than 0");
         uint256 balance = token.balanceOf(msg.sender);
         require(balance > amount,"not enough balance");
-        token._burn(msg.sender,balance);
+        token.burn(msg.sender,balance);
 
-        priceRatio = (tokenPrice*pctPrecision)/getCollateralPrice();
-        pctOfCollateral = ( getCollateralPrice() * priceRatio ) /pctPrecision;
-        amountOfCollateral = pctOfCollateral * balance;
-        safeTransfer(msg.sender, amountOfCollateral);
+        uint256 priceRatio = (getTokenPrice()*pctPrecision)/getCollateralPrice();
+        uint256 pctOfCollateral = ( getCollateralPrice() * priceRatio ) /pctPrecision;
+        uint256 amountOfCollateral = pctOfCollateral * balance;
+        this.transfer(msg.sender, amountOfCollateral);
     }
 
-    function liquidate(uint256 amount)external returns (uint256){
+    // Calculate how much percents of the total supply this sender owns and withdraw the same amount of percents  minus the penalty from the collateral.
+    // Example:
+    // token totalSupply is 10000, 
+    // collateral totalSupply is 1000
+    // sender owns 1000 (10% of token totalSupply)
+    // with penatly 0% sender can withdraw 100 collateral(10% of collateral total supply)
+    // with penatly 10% sender can withdraw 90 collateral
+    function liquidate()external {
         require(collateralUtilization() > collateralThreshold,"can run a liquidation only when collateral utilizatoin is above the collateral threshold");
-        // Calculate how much percents of the total supply this sender owns and withdraw the same amount or percents from the collateral.
-        // Example:
-        // token totalSupply is 10000, 
-        // collateral totalSupply is 1000
-        // sender owns 100 (1% of token totalSupply)
-        // sender can withdraw 10 collateral(1% of collateral total supply)
+        uint256 totalSupplyRatio = (this.totalSupply()*pctPrecision)/token.totalSupply()  ;
+        uint256 collateralAmount = (this.totalSupply()*totalSupplyRatio) / pctPrecision;
+        uint256 collateralAmountP = collateralAmount- ((collateralAmount *liquidationPenatly) /pctPrecision);
+        this.transfer(msg.sender, collateralAmountP);
     }
 
     // Increases the total supply and sends the new tokens to the inflation beneficiary.
     function updateInflation() public {
         require((block.timestamp - inflLastUpdate)> inflPeriod,"inflation period hasn't passed");
-        daysPassed = (block.timestamp - inflLastUpdate)/inflPeriod;
-        inflIncrease = inflRate * daysPassed;
+        uint256 inflIncrease = _inflation(inflLastUpdate);
 
         // Mint tokens amount that equals to the inflation increase.
         // inflIncrease is in percents basis points. 100% = 10 000 basis points.
         // TODO add tests for this.
-        tokensToMint = (token.totalSupply()*inflIncrease)/pctPrecision;
+        uint256 tokensToMint = (token.totalSupply()*inflIncrease) / pctPrecision;
 
         // Can be zero when token.totalSupply()*inflIncrease is below pctPrecision;
         if (tokensToMint>0){
             // Mint the amount of inflation to the inflBeneficiary address.
-            token.mint(tokensToMint, inflBeneficiary);
+            token.mint(inflBeneficiary,tokensToMint);
             inflLastUpdate = block.timestamp;
         }
+    }
+
+    // Returns the inflation in percent with pctPrecision granularity.
+    function _inflation(uint256 since) internal view returns(uint256){
+        uint256 inflUnitPassed = (block.timestamp - since)/inflPeriod;
+        return inflRate * inflUnitPassed;
     }
     
     // The maximum amount in percentage of collateral utilization.
     // Percents returned are with pctPrecision granularity.
     function collateralUtilization() public view returns (uint256){
-        return  ( (tokenPrice * token.totalSupply() * pctPrecision)  / (getCollateralPrice() * totalSupply() ) ) ; // TODO add test.
+        require(token.totalSupply()>0,"token total supply is zero");
+        require(this.totalSupply()>0,"collateral total supply is zero");
+        return  ( ( getTokenPrice() * token.totalSupply() * pctPrecision)  / (getCollateralPrice() * this.totalSupply() ) ) ; // TODO add test.
     }
 
-    // Returns the current collateral price in USD upscaled to e18 precision.
-    function getCollateralPrice() public returns (uint256){
-        (didGet, collateralPrice, timestamp) = getCurrentValue(collateralID);
+    // Returns the collateral price in USD upscaled to e18 precision.
+    function getCollateralPrice() public view returns (uint256){
+        (bool   didGet, uint256 collateralPrice,uint256  timestamp) = getDataBefore(collateralID, block.timestamp-collateralPriceAge);
         require(didGet,"getting oracle price");
         require(!isInDispute(collateralID,timestamp),"current value is in dispute");
 
-        return collateralPrice*collateralPricePrecision;
+        return collateralPrice*(1e18/collateralPricePrecision);
     }
 
+    // Returns the current token price in USD reduced by the current inflation.
+    function getTokenPrice() public view returns (uint256){
+        uint256 totalInflation =  _inflation(creationTimestamp);
+        return 1e18-(1e18*totalInflation)/pctPrecision;
+    }
+
+    // WARNING You would usually want to put this through a vote from the token holders
+    // or the collateral provider can set it very low and drain all collateral.
+    // Usually the owner should be another contract so that it is allowed to change it only after a vote from the token holders.
     // Sets the collateral threshold with pctPrecision granulatiry.
-    // TODO Should go trough a vote. Otherwise the provider can set it very low and drain all collateral.
-    function setCollateralThreshold(uint256 percent) external onlyAdmin{
-        require(percent>0 && percent < pctPrecision,"collateral threshold not within allowed limits");
+    function setCollateralThreshold(uint256 percent) external onlyAdmin withinPctRange(percent){
         collateralThreshold = percent;
     }
 
+    function setCollateralPriceAge(uint256 age)public onlyAdmin{
+            collateralPriceAge = age;
+    }
 
     // The max minted tokens can be up to the max utulization threshold.
     // Noone should be allowed to mint above the utilizationThreshold otherwise can drain the pool.
     function mint(uint256 amount, address to ) public onlyAdmin {
-        token.mint(amount, to); // TODO Add e2e test to ensure that this is reverted when above the collateral threshold.
+        token.mint(to,amount); // TODO Add e2e test to ensure that this is reverted when above the collateral threshold.
         require(collateralUtilization() < collateralThreshold,"minting tokens will cause collateral utilization to go above the allowed threshold");
     }
 }
--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,117 +2,18 @@
 
 pragma solidity ^0.7.0;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Token  {
-    using SafeMath for uint256;
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
-    mapping (address => uint256) private _balances;
-    mapping (address => mapping (address => uint256)) private _allowances;
-
-    uint256 private _totalSupply;
-
-    string private _name;
-    string private _symbol;
-    uint8 private _decimals;
-
-    constructor (string memory n, string memory s) {
-        _name = n;
-        _symbol = s;
-        _decimals = 18;
-        _mint(msg.sender, 1000);
-    }
-    function name() public view returns (string memory) {
-        return _name;
+// The contract is also an ERC20 token which holds the collateral currency.
+// It also holds the semi stable token state inside the `token` variable.
+contract Token is ERC20 {
+    function burn(address account, uint256 amount) public {
+        _burn(account, amount);
     }
 
-
-    function symbol() public view returns (string memory) {
-        return _symbol;
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
     }
 
-    function decimals() public view returns (uint8) {
-        return _decimals;
-    }
-
-    function totalSupply() public view returns (uint256) {
-        return _totalSupply;
-    }
-
-    function balanceOf(address account) public view returns (uint256) {
-        return _balances[account];
-    }
-
-    function transfer(address recipient, uint256 amount) public virtual returns (bool) {
-        _transfer(msg.sender, recipient, amount);
-        return true;
-    }
-
-  
-    function allowance(address owner, address spender) public view virtual returns (uint256) {
-        return _allowances[owner][spender];
-    }
-
-
-    function approve(address spender, uint256 amount) public virtual returns (bool) {
-        _approve(msg.sender, spender, amount);
-        return true;
-    }
-
-  
-    function transferFrom(address sender, address recipient, uint256 amount) public virtual returns (bool) {
-        _transfer(sender, recipient, amount);
-        _approve(sender, msg.sender, _allowances[sender][msg.sender].sub(amount, "ERC20: transfer amount exceeds allowance"));
-        return true;
-    }
-
-    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
-        _approve(msg.sender, spender, _allowances[msg.sender][spender].add(addedValue));
-        return true;
-    }
-
-
-    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
-        _approve(msg.sender, spender, _allowances[msg.sender][spender].sub(subtractedValue, "ERC20: decreased allowance below zero"));
-        return true;
-    }
-
-
-    function _transfer(address sender, address recipient, uint256 amount) internal virtual {
-        require(sender != address(0), "ERC20: transfer from the zero address");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
-
-        _balances[sender] = _balances[sender].sub(amount, "ERC20: transfer amount exceeds balance");
-        _balances[recipient] = _balances[recipient].add(amount);
-        emit Transfer(sender, recipient, amount);
-    }
-
-
-    function _mint(address account, uint256 amount) internal virtual {
-        require(account != address(0), "ERC20: mint to the zero address");
-
-        _totalSupply = _totalSupply.add(amount);
-        _balances[account] = _balances[account].add(amount);
-        emit Transfer(address(0), account, amount);
-    }
-
-    function _burn(address account, uint256 amount) internal virtual {
-        require(account != address(0), "ERC20: burn from the zero address");
-
-        _balances[account] = _balances[account].sub(amount, "ERC20: burn amount exceeds balance");
-        _totalSupply = _totalSupply.sub(amount);
-        emit Transfer(account, address(0), amount);
-    }
-
-
-    function _approve(address owner, address spender, uint256 amount) internal virtual {
-        require(owner != address(0), "ERC20: approve from the zero address");
-        require(spender != address(0), "ERC20: approve to the zero address");
-
-        _allowances[owner][spender] = amount;
-        emit Approval(owner, spender, amount);
-    }
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 }
