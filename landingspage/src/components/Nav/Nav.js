6638ac333e2702df8f26e55d7dd81d4264dede2c
bennisan86 <info@bennisan.be>
2021-01-18 16:48:31 +0100

    sync landingpage

---
+++ landingspage/src/components/Nav/Nav.js
@@ -0,0 +1,75 @@
+import React, {useState} from 'react';
+import './Nav.scss';
+import Icons from '../../Icons';
+import { slide as Menu } from 'react-burger-menu';
+import { useMediaQuery } from 'react-responsive';
+
+const Nav = (props) => {
+    const isMobile = useMediaQuery({query: '(max-width: 810px) and (-webkit-min-device-pixel-ratio: 2)'});
+
+   
+    const [menuOpen,setMenuOpen] = useState(false);
+
+    const handleClick = (ref) => {
+        if(ref === "RefA"){
+            props.onClickRefA();
+        }
+        if(ref === "RefB"){
+            props.onClickRefB();
+        }
+        setMenuOpen(false);
+    }
+
+    const handleStateChange = (ref) => {
+        if(ref.isOpen){
+            setMenuOpen(true);
+        }        
+    }
+
+    return (
+        <div className="Nav">
+        {isMobile ?
+        <div className="Nav_inner">
+            <Menu
+                right
+                customBurgerIcon={ <Icons.Hamburger /> }
+                customCrossIcon={ <Icons.Closer /> }
+                isOpen={menuOpen}
+                onStateChange={(ref) => handleStateChange(ref)}>
+                <a href="https://tellor.io" target="_blank" rel="noopener noreferrer">
+                    <Icons.Github className="Github" fill="white" height="30px" width="30px" />
+                </a>
+                <a href="https://tellor.io" target="_blank" rel="noopener noreferrer">whitepaper</a>
+                <div onClick={() => handleClick("RefA")}><p>how does it work</p></div>
+                <div onClick={() => handleClick("RefB")}><p>contact</p></div>
+            </Menu>
+            <div className="LogoBox">
+                <p><a href="https://tellor.io" className="tellorLink" target="_blank" rel="noopener noreferrer">tellor </a>presents</p>
+                <Icons.Logo className="Logo" height="18px" width="18px" />
+                <p>CHORUS</p>
+            </div>
+            <div className="flexer"></div>
+        </div>
+        :
+        <div className="Nav_inner">
+            <div className="LogoBox">
+                <p><a href="https://tellor.io" className="tellorLink" target="_blank" rel="noopener noreferrer">tellor </a>presents</p>
+                <Icons.Logo className="Logo" height="18px" width="18px" />
+                <p>CHORUS</p>
+            </div>
+            <div className="flexer"></div>
+            <div className="Menu_Total">
+                <a href="https://tellor.io" target="_blank" rel="noopener noreferrer">whitepaper</a>
+                <div onClick={props.onClickRefA}><p>how does it work</p></div>
+                <div onClick={props.onClickRefB}><p>contact</p></div>
+                <a href="https://tellor.io" target="_blank" rel="noopener noreferrer">
+                    <Icons.Github className="Github" height="26px" width="26px" />
+                </a>
+            </div>
+        </div>
+}
+        </div>
+    );
+}
+
+export default Nav;
---
+++ landingspage/src/components/Steps/Steps.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './Steps.scss';
+import ChorusButton from '../ChorusButton/ChorusButton';
+import { useMediaQuery } from 'react-responsive';
+
+function Steps() {
+  const isMobile = useMediaQuery({query: '(max-width: 480px)'})
+
+  return (
+    <div className="Steps">
+      <p className="StepsFirst">The flow of the <span className="bold">Chorus</span> protocol {isMobile?<br/>:null}is as such:</p>
+      <div className="Step">
+        <p>Issuing party deploys an <span className="bold">Anthem</span></p>
+        <svg xmlns="http://www.w3.org/2000/svg" width="12.29" height="6.88" viewBox="0 0 12.29 6.88">
+          <path d="M2642.013,1902.739s4.9,5.825,5.2,6.213a1.818,1.818,0,0,0,.181.2c2.517-1.639,5.843-5.788,6.908-6.7a5.561,5.561,0,0,0-1.959-.165c-1.277.072-8.187.21-8.826.21A6.868,6.868,0,0,0,2642.013,1902.739Z" transform="translate(-2642.013 -1902.271)"/>
+        </svg>
+      </div>
+      <div className="Step">
+        <p>Issuing party is owner of this  <span className="bold">Anthem</span> or transfers ownership</p>
+        <svg xmlns="http://www.w3.org/2000/svg" width="12.29" height="6.88" viewBox="0 0 12.29 6.88">
+          <path d="M2642.013,1902.739s4.9,5.825,5.2,6.213a1.818,1.818,0,0,0,.181.2c2.517-1.639,5.843-5.788,6.908-6.7a5.561,5.561,0,0,0-1.959-.165c-1.277.072-8.187.21-8.826.21A6.868,6.868,0,0,0,2642.013,1902.739Z" transform="translate(-2642.013 -1902.271)"/>
+        </svg>
+      </div>
+      <div className="Step">
+        <p>Owner set variables and locks collateral of their chosen token</p>
+        <svg xmlns="http://www.w3.org/2000/svg" width="12.29" height="6.88" viewBox="0 0 12.29 6.88">
+          <path d="M2642.013,1902.739s4.9,5.825,5.2,6.213a1.818,1.818,0,0,0,.181.2c2.517-1.639,5.843-5.788,6.908-6.7a5.561,5.561,0,0,0-1.959-.165c-1.277.072-8.187.21-8.826.21A6.868,6.868,0,0,0,2642.013,1902.739Z" transform="translate(-2642.013 -1902.271)"/>
+        </svg>
+      </div>
+      <div className="Step">
+        <p>Owner can now issue <span className="bold">Notes</span> as community currency</p>
+        <svg xmlns="http://www.w3.org/2000/svg" width="12.29" height="6.88" viewBox="0 0 12.29 6.88">
+          <path d="M2642.013,1902.739s4.9,5.825,5.2,6.213a1.818,1.818,0,0,0,.181.2c2.517-1.639,5.843-5.788,6.908-6.7a5.561,5.561,0,0,0-1.959-.165c-1.277.072-8.187.21-8.826.21A6.868,6.868,0,0,0,2642.013,1902.739Z" transform="translate(-2642.013 -1902.271)"/>
+        </svg>
+      </div>
+      <p><span className="bold">Notes</span> are subject to inflation rate, new <span className="bold">Notes</span> are issued to Inflation Beneficiary</p>
+      <div className="readwhitepaper">
+        <p>Want to know more?</p>
+        <ChorusButton txt={"Read the whitepaper"}/>
+      </div>
+    </div>
+  );
+}
+
+export default Steps;
