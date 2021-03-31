bf40f5a8982c617a557b420da16759d188999018
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2021-03-31 19:15:50 +0300

    WIP
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

--- contracts/ChorusTest.sol
+++ contracts/ChorusTest.sol
@@ -7,7 +7,9 @@ import "./ERC20.sol";
 
 contract ChorusTest is Chorus {
     ITellor public oracle = ITellor(new MockOracle());
-    ERC20 public collateral = new ERC20("Ethereum", "ETH");
+    Mintable public collateral = new Mintable("Ethereum", "ETH");
+
+    uint256 public constant collateralDeposit = 1e19;
 
     // 10% effective inf rate.
     constructor()
@@ -22,11 +24,12 @@ contract ChorusTest is Chorus {
             address(0x1)
         )
     {
-        admin = address(0x0);
+        admin = 0x0e93f23278161bCa6F22D6268ca44691042ed437;
+        collateral.mint(admin, collateralDeposit);
     }
 
-    function echidna_colateral_deposit() public view returns (bool) {
-        return collateralToken.balanceOf(address(this)) == 0;
+    function echidna_colateral_balance() public view returns (bool) {
+        return collateralToken.balanceOf(admin) == collateralDeposit;
     }
 
     function echidna_total_supply() public view returns (bool) {
