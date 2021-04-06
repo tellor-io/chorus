4b2d412d25ab628ed3184575b5be70b52e2d9347
themandalore <fett.nicholas@yahoo.com>
2021-04-06 11:31:39 -0400

    oracle getter comments

--- contracts/OracleGetter.sol
+++ contracts/OracleGetter.sol
@@ -11,71 +11,14 @@ import "./interfaces/ITellor.sol";
 contract OracleGetter {
     ITellor private tellor;
 
+    /**
+     * @dev Constructor for the oracle getters
+     * @param _tellor the tellor address
+     */
     constructor(address payable _tellor) {
         tellor = ITellor(_tellor);
     }
-
-    // solhint-disable-next-line
-    function _getIndexForDataBefore(uint256 _requestId, uint256 _timestamp)
-        internal
-        view
-        returns (bool found, uint256 index)
-    {
-        uint256 _count = tellor.getNewValueCountbyRequestId(_requestId);
-        if (_count > 0) {
-            uint256 middle;
-            uint256 start = 0;
-            uint256 end = _count - 1;
-            uint256 _time;
-
-            //Checking Boundaries to short-circuit the algorithm
-            _time = tellor.getTimestampbyRequestIDandIndex(_requestId, start);
-            if (_time >= _timestamp) return (false, 0);
-            _time = tellor.getTimestampbyRequestIDandIndex(_requestId, end);
-            if (_time < _timestamp) return (true, end);
-
-            //Since the value is within our boundaries, do a binary search
-            while (true) {
-                middle = (end - start) / 2 + 1 + start;
-                _time = tellor.getTimestampbyRequestIDandIndex(
-                    _requestId,
-                    middle
-                );
-                if (_time < _timestamp) {
-                    //get imeadiate next value
-                    uint256 _nextTime =
-                        tellor.getTimestampbyRequestIDandIndex(
-                            _requestId,
-                            middle + 1
-                        );
-                    if (_nextTime >= _timestamp) {
-                        //_time is correct
-                        return (true, middle);
-                    } else {
-                        //look from middle + 1(next value) to end
-                        start = middle + 1;
-                    }
-                } else {
-                    uint256 _prevTime =
-                        tellor.getTimestampbyRequestIDandIndex(
-                            _requestId,
-                            middle - 1
-                        );
-                    if (_prevTime < _timestamp) {
-                        // _prevtime is correct
-                        return (true, middle - 1);
-                    } else {
-                        //look from start to middle -1(prev value)
-                        end = middle - 1;
-                    }
-                }
-                //We couldn't found a value
-                //if(middle - 1 == start || middle == _count) return (false, 0);
-            }
-        }
-        return (false, 0);
-    }
-
+    
     /**
      * @dev Allows the user to get the first value for the requestId before the specified timestamp
      * @param _requestId is the requestId to look up the value for
@@ -102,5 +45,65 @@ contract OracleGetter {
         //If value is diputed it'll return zero
         if (_value > 0) return (true, _value, _time);
         return (false, 0, 0);
+    }
+
+    /**
+     * @dev Internal function to get the index of the Tellor data
+     * @param _requestId requestID for Tellor data feed
+     * @param _timestamp timestamp of request
+     */
+    function _getIndexForDataBefore(uint256 _requestId, uint256 _timestamp)
+        internal
+        view
+        returns (bool, uint256)
+    {
+        uint256 _count = tellor.getNewValueCountbyRequestId(_requestId);
+        if (_count > 0) {
+            uint256 _middle;
+            uint256 _start = 0;
+            uint256 _end = _count - 1;
+            uint256 _time;
+            //Checking Boundaries to short-circuit the algorithm
+            _time = tellor.getTimestampbyRequestIDandIndex(_requestId, _start);
+            if (_time >= _timestamp) return (false, 0);
+            _time = tellor.getTimestampbyRequestIDandIndex(_requestId, _end);
+            if (_time < _timestamp) return (true, _end);
+            //Since the value is within our boundaries, do a binary search
+            while (true) {
+                _middle = (_end - _start) / 2 + 1 + _start;
+                _time = tellor.getTimestampbyRequestIDandIndex(
+                    _requestId,
+                    _middle
+                );
+                if (_time < _timestamp) {
+                    //get imeadiate next value
+                    uint256 _nextTime =
+                        tellor.getTimestampbyRequestIDandIndex(
+                            _requestId,
+                            _middle + 1
+                        );
+                    if (_nextTime >= _timestamp) {
+                        return (true, _middle);//_time is correct
+                    } else {
+                        //look from middle + 1(next value) to end
+                        _start = _middle + 1;
+                    }
+                } else {
+                    uint256 _prevTime =
+                        tellor.getTimestampbyRequestIDandIndex(
+                            _requestId,
+                            _middle - 1
+                        );
+                    if (_prevTime < _timestamp) {
+                        // _prevtime is correct
+                        return (true, _middle - 1);
+                    } else {
+                        //look from start to middle -1(prev value)
+                        _end = _middle - 1;
+                    }
+                }
+            }
+        }
+        return (false, 0);
     }
 }
