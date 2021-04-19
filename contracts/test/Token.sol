06999d5f1dc1ed90c7d296571dc403997bb144e0
themandalore <fett.nicholas@yahoo.com>
2021-04-19 14:11:36 -0400

    Create Token.sol

---
+++ contracts/test/Token.sol
@@ -0,0 +1,14 @@
+// The contract is also an ERC20 token which holds the collateral currency.
+// It also holds the semi stable token state inside the `token` variable.
+contract Token is ERC20 {
+    function burn(address account, uint256 amount) external {
+        _burn(account, amount);
+    }
+
+    function mint(address account, uint256 amount) external {
+        _mint(account, amount);
+    }
+
+    // solhint-disable-next-line no-empty-blocks
+    constructor(string memory n, string memory s) ERC20(n, s) {}
+}
